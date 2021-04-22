155338edcceb27e0c55336c0e2ec8ff40b860b67
Dean <deansingleton1999@gmail.com>
2021-04-23 00:19:07 +0100

    icecandidate event not firing

--- client/index.js
+++ client/index.js
@@ -7,6 +7,23 @@ class WebSocketWrapper {
         this.conn.addEventListener('message', this.onMessage.bind(this));
         this.conn.addEventListener('open', this.onOpen.bind(this));
         this.conn.addEventListener('close', this.onClose);
+
+        this.peerConnection.addEventListener('icecandidate', this.newIceCandidate.bind(this));
+        this.peerConnection.addEventListener('connectionstatechange', this.connectionChange.bind(this));
+    }
+
+    newIceCandidate(event) {
+        console.log('yo!!!!');
+        if (event.candidate) {
+            this.send({'new-ice-candidate': event.candidate});
+        }
+    }
+
+    connectionChange(event) {
+        console.log('yo!!! anyone?');
+        if (this.peerConnection.connectionState === 'connected') {
+            console.log('we have peers!');
+        }
     }
 
     send(data) {
@@ -15,8 +32,19 @@ class WebSocketWrapper {
         this.conn.send(message);
     }
 
-    onMessage(message) {
+    async onMessage(message) {
         let data = JSON.parse(message.data);
+
+        if (data.iceCandidate) {
+            console.log('yo new guy');
+            try {
+                await this.peerConnection.addIceCandidate(data.iceCandidate);
+            } catch (error) {
+                console.error('Error adding received ice candidate', e);
+            }
+
+            return false;
+        }
 
         if (data.type == 'answer') {
             console.log('got an answer');
@@ -32,6 +60,7 @@ class WebSocketWrapper {
     }
 
     async handleAnswer(answer) {
+        console.log(answer);
         const remoteDesc = new RTCSessionDescription(answer);
         await this.peerConnection.setRemoteDescription(remoteDesc);
     }
@@ -45,7 +74,6 @@ class WebSocketWrapper {
     }
 
     async createOffer() {
-        console.log('creating offer');
         const offer = await this.peerConnection.createOffer();
 
         await this.peerConnection.setLocalDescription(offer);
@@ -63,5 +91,3 @@ class WebSocketWrapper {
 }
 
 let webSocket = new WebSocketWrapper('ws://localhost:3001');
-
-// webSocket.createOffer();
